6962d76a2d759a4cd14dd5b8eee227d52cb1fb25
Aastha <asingla1101@gmail.com>
2025-08-02 12:58:54 +0530

    added HTTPExceptions

--- fast_api/books_2.py
+++ fast_api/books_2.py
@@ -1,7 +1,7 @@
 # --------TO RUN--------
 # uvicorn books_2:app --reload -> click on the links -> change the url = {url}/docs (for swagger UI)
 from typing import Optional
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
 
 app = FastAPI()
@@ -60,6 +60,7 @@ async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
+    raise HTTPException(status_code=404, detail='Item not found')
 
 @app.get("/books/")
 async def read_book_by_rating(book_rating: int = Query(gt=0,lt=6)):
@@ -95,13 +96,21 @@ def find_book_id(book: Book):
 
 @app.put("/books/update_book")
 async def update_book(book: BookRequest):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
 
 @app.delete("/books/{book_id}")
 async def delete_book(book_id: int = Path(gt=0)):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
+            book_changed = True
             break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
